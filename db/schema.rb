10b2f89ff7251efc32100d09010125686f840525
Ruekompa <kompinski@gmail.com>
2016-04-06 03:47:17 -0400

    Track creation naming defaults

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402204315) do
+ActiveRecord::Schema.define(version: 20160406065001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -130,6 +130,7 @@ ActiveRecord::Schema.define(version: 20160402204315) do
     t.string   "album_id"
     t.string   "file_name"
     t.integer  "project_id"
+    t.integer  "row_order"
   end
 
   add_index "tracks", ["album_id"], name: "index_tracks_on_album_id", using: :btree
