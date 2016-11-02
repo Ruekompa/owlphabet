cc7da1d98dda9e28d56546c56039466ceaca31b1
Ruekompa <russell@signalmtn.io>
2016-11-02 14:01:07 -0400

    producition

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'owlphabet.com' }
 
   config.action_mailer.delivery_method = :smtp
 
@@ -89,5 +89,5 @@ Rails.application.configure do
   :port => 587,
   :authentication => :plain,
    }
-   
+
 end
