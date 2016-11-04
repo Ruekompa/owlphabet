3e0efadfd4af17c81083225f7636b69c98dff4d6
Ruekompa <russell@signalmtn.io>
2016-11-04 15:07:30 -0400

    fog goog storage uplaods cant work with rest of app without errors

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
-CarrierWave.configure do |config|
+# CarrierWave.configure do |config|
 
 # def store_dir
-#   "/PATH/RAILSAPPLICATION/uploads/#{model.id}"
+#   "/PATH/RAILSAPPLICATION/uploads/songs/#{model.id}"
 # end
 
 # def cache_dir
-#   "/PATH/RAILSAPPLICATION/tmp/uploads/cache/#{model.id}"
+#   "/PATH/RAILSAPPLICATION/uploads/songs/tmp/cache/#{model.id}"
 # end
 
-  # config.fog_provider = 'fog/google'                       
-  # config.fog_credentials = {
-  #   provider:                         'Google',
-  #   google_storage_access_key_id:     'GOOG3GITCFR74T3GSX3M',
-  #   google_storage_secret_access_key: 'X9Wuyz6GE25MNcfhEHcavM2Jc2pXcq9rPfQeymhv'
-  # }
-  # config.fog_directory = 'nestformusic'
-end
+#   config.fog_provider = 'fog/google'                       
+#   config.fog_credentials = {
+#     provider:                         'Google',
+#     google_storage_access_key_id:     'GOOGBNL7C3XGDT6GWLPO',
+#     google_storage_secret_access_key: 'wayBZqlo3gpMPU5r8Ezek53eqUM1hL1RVBMOakvu'
+#   }
+#   config.fog_directory = 'owlphabet-music.appspot.com'
+# end
