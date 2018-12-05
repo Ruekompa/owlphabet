61c240abc48948f7605d7481956090a55550f4cd
Russell Kompinski <rskompinski@gmail.com>
2018-12-05 10:43:53 -0500

    Update carrierwave.rb

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,18 @@
+# CarrierWave.configure do |config|
+
+# def store_dir
+#   "/PATH/RAILSAPPLICATION/uploads/songs/#{model.id}"
+# end
+
+# def cache_dir
+#   "/PATH/RAILSAPPLICATION/uploads/songs/tmp/cache/#{model.id}"
+# end
+
+#   config.fog_provider = 'fog/google'                       
+#   config.fog_credentials = {
+#     provider:                         'Google',
+#     google_storage_access_key_id:     <%= ENV['GOOGLE_STORAGE_KEY_ID'] %>,
+#     google_storage_secret_access_key: <%= ENV['GOOGLE_SECRET_ACCESS_KEY'] %>
+#   }
+#   config.fog_directory = 'owlphabet-music.appspot.com'
+# end
